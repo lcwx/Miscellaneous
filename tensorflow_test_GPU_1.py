3d6f7ac94a80c539aa7be03ba5086977cca79492
星辰漫游者 <39415743+xcmyz@users.noreply.github.com>
2019-02-08 10:23:59 +0800

    Add files via upload

---
+++ tensorflow_test_GPU_1.py
@@ -0,0 +1,61 @@
+# import tensorflow as tf
+
+# a = tf.Variable(2, name = "a")
+# b = tf.Variable(2, name = "b")
+# # a = tf.get_variable("a". shape=(2, 2))
+# # b = tf.get_variable("b". shape=(2, 2))
+
+# init = tf.global_variables_initializer()
+
+# with tf.Session()as sess:
+#     sess.run(init)
+#     for i in range(100000000000):
+#         # print(sess.run(a + b))
+#         sess.run(a + b)
+#         if i == 1000:
+#             print("######################")
+
+import tensorflow as tf
+# from tensorflow.python.client import device_lib
+import numpy as np
+import random
+import time
+import os
+
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+# print(device_lib.list_local_devices())
+
+matrix1 = np.array([])
+matrix2 = np.array([])
+
+for i in range(1000):
+    matrix1 = np.append(matrix1, [random.randint(20, 600) for i in range(1000)])
+    matrix2 = np.append(matrix2, [random.randint(1, 500) for i in range(1000)])
+
+matrix1 = matrix1.reshape(1000, 1000)
+matrix2 = matrix2.reshape(1000, 1000)
+# matrix2 = tf.matrix_diag(matrix2)
+
+# print(matrix1)
+# print(matrix2)
+# with tf.device('/cpu:0'):
+with tf.Session() as sess:
+    m1 = tf.convert_to_tensor(matrix1)
+    m2 = tf.convert_to_tensor(matrix2)
+    out_ = tf.matrix_diag(matrix2)
+    # print(out_)
+
+    out = tf.multiply(m1, m2)
+    t1 = time.clock()
+    for i in range(10000):
+        # print(sess.run(out))
+        # out = tf.multiply(m1, m2)
+        # matrix2 = tf.matrix_diag(matrix2)
+        sess.run(out)
+        # sess.run(out_)
+        if i % 1000 == 0:
+            print("#############")
+    t2 = time.clock()
+    t = t2 - t1
+    print(t)
+    print("Time used: %.3f" % (t))
